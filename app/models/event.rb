59f008ade7036c9f5690590da4e8344a708b4731
duvin <u_win@mail.ru>
2016-11-30 00:30:20 +0700

    добавил события

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :game
   KIND_OF_EVENT = %w(goal substitute foul yellow_card red_card).freeze
+
+  def logo
+  	return "/yellow_card.jpeg" if self.kind == "yellow_card"
+  	return "/red_card.jpeg" if self.kind == "red_card"
+  	return "/goal.jpeg" if self.kind == "goal"
+  end
 end
