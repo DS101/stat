3937c358c9858756646653b86d21992f348fc24a
Дмитрий Увин <u_win@mail.ru>
2016-11-30 08:43:37 +0600

    сортировка событий

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   belongs_to :home_team, :class_name => :team
   belongs_to :away_team, :class_name => :team
-  has_many :events
+  has_many :events, -> { order time: :asc } 
 
   def title
     return "#{self.date.strftime("%d-%m-%Y")} #{Team.find(self.home_team_id).title} #{self.home_team_goal}:#{self.away_team_goal} #{Team.find(self.away_team_id).title}"
